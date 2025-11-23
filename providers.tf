33fd0fefb22f8a481f06e936ba2a51b389146ffb
Brandon Jacklyn <brandonjacklyn@live.ca>
2025-11-23 14:14:04 -0800

    Add k8s nginx-ingress and tls cert management

--- providers.tf
+++ providers.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.27"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
   
@@ -5,6 +22,35 @@ provider "aws" {
     tags = {
       Environment = "Production"
       Owner       = "DevOps"
+    }
+  }
+}
+
+# --- DYNAMIC CLUSTER AUTHENTICATION ---
+# We use the EKS module outputs to configure the K8s provider.
+# This ensures Terraform creates the cluster FIRST, then logs in to it.
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the aws cli to be installed locally where you run terraform
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     }
   }
 }
